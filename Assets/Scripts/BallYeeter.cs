a3c2d1f54b849a7294a4a607f7457dfdb75e881f
SzyZu <skwarek116@gmail.com>
2024-01-16 17:55:07 +0100

    feat: Camera Rotation

--- Assets/Scripts/BallYeeter.cs
+++ Assets/Scripts/BallYeeter.cs
@@ -5,10 +5,17 @@ public class BallYeeter : MonoBehaviour
 {
     [SerializeField] float forceValue;
     [SerializeField] private Rigidbody ballPrefab;
+    public int sensX;
+    public int sensY;
+
+    private float xRotation;
+    private float yRotation;
     
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -16,9 +23,19 @@ public class BallYeeter : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector3 yeetPower = Vector3.forward * forceValue;
+            Vector3 yeetPower = transform.forward * forceValue;
             Rigidbody instantiated = Instantiate(ballPrefab, transform.position, Quaternion.identity);
             instantiated.AddForce(yeetPower);
         }
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }

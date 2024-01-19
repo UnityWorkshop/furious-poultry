c80fac901ff24dae8bc28b733eded16876136713
SzyZu <skwarek116@gmail.com>
2024-01-19 17:20:25 +0100

    fix: Balls dont spawn inside Player, spawn position can be adjusted

--- Assets/Scripts/BallYeeter.cs
+++ Assets/Scripts/BallYeeter.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BallYeeter : MonoBehaviour
 {
-    [SerializeField] float forceValue;
+    [SerializeField] private float forceValue;
     [SerializeField] private Rigidbody ballPrefab;
+    [SerializeField] private Transform yeetPos;
+    
     public int sensX;
     public int sensY;
 
-    private float xRotation;
-    private float yRotation;
+    private float _xRotation;
+    private float _yRotation;
     
     // Start is called before the first frame update
     void Start()
@@ -24,18 +26,18 @@ public class BallYeeter : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Vector3 yeetPower = transform.forward * forceValue;
-            Rigidbody instantiated = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+            Rigidbody instantiated = Instantiate(ballPrefab, yeetPos.position, Quaternion.identity);
             instantiated.AddForce(yeetPower);
         }
 
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
+        _yRotation += mouseX;
+        _xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
     }
 }

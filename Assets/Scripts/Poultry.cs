8cf253bf21448445decdd79b5ef415a899805469
I-Protecc <ryanbaumeister@googlemail.com>
2024-01-30 17:53:08 +0100

    feat: attempting to work on camera locking

--- Assets/Scripts/Poultry.cs
+++ Assets/Scripts/Poultry.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class Poultry : MonoBehaviour
 {
     [SerializeField] private int damage;
+    public bool isOnGround = false;
 
     private void OnCollisionEnter(Collision collision)
     {
         Enemies collidedEnemyScript = collision.gameObject.GetComponent<Enemies>();
         if (collidedEnemyScript is not null)
             collidedEnemyScript.Damage(damage);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //Debug.Log("hit ground"); 
+            isOnGround = true;
+            //death here
+        }
     }
 }

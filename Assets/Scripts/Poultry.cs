95e641c86e761cc9cc92e868004f492791b8eb25
I-Protecc <ryan.baumeister@gmail.com>
2024-02-06 18:01:35 +0100

    feat: poultry hp, health decay

--- Assets/Scripts/Poultry.cs
+++ Assets/Scripts/Poultry.cs
@@ -6,18 +6,44 @@ using UnityEngine;
 public class Poultry : MonoBehaviour
 {
     [SerializeField] private int damage;
-    public bool isOnGround = false;
+    [SerializeField] private float health = 100f;
+    [SerializeField] private bool isOnGround = false;
+    [SerializeField] private bool hasCollided = false;
+    [SerializeField] private float decayTickDamage = 1f / 60f;
+
+    // todo: timed life -> reduce hp by x every second after collision
+    private void FixedUpdate()
+    {
+        if (hasCollided)
+        {
+            health -= decayTickDamage;
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        hasCollided = true;
         Enemies collidedEnemyScript = collision.gameObject.GetComponent<Enemies>();
         if (collidedEnemyScript is not null)
+        {
             collidedEnemyScript.Damage(damage);
+            health -= damage;
+        }
+            
         if (collision.gameObject.CompareTag("Ground"))
         {
             //Debug.Log("hit ground"); 
             isOnGround = true;
             //death here
         }
+
+        
+            
+        
     }
 }

ffc78baecfca726951c3a27914a4931de81a77fe
dodofilet <tortenspam@outlook.com>
2024-02-06 18:00:07 +0100

    feat:started implementing being able to change paths

--- Assets/Scripts/ZeplinAgent.cs
+++ Assets/Scripts/ZeplinAgent.cs
@@ -12,6 +12,9 @@ public class ZeplinAgent : MonoBehaviour
     private NavMeshAgent _navMeshAgent;
     private Transform currentTarget;
     private int currentTargetIndex;
+    private List<Transform> path1;
+    private List<Transform> path2;
+    private List<Transform> currentPath;
     [SerializeField] private float stoppingDistance;
     [SerializeField] private List<Transform> targets ;
     // Start is called before the first frame update
@@ -20,26 +23,73 @@ public class ZeplinAgent : MonoBehaviour
         _navMeshAgent = GetComponent<NavMeshAgent>();
         if (!targets.Any())
             throw new ArgumentException("you stupid");
+        fillPaths();
         ChangeTarget();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentTarget is null)
+        {
+            throw new ArgumentException("whyyyyyyyyyy");
+        }
         if (Vector3.Distance(transform.position, currentTarget.position )<= stoppingDistance)
             ChangeTarget();
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentPath=changePath();
+        }
     }
 
     void ChangeTarget()
     {
-        if (currentTarget is null || currentTargetIndex>=targets.Count)
+        if (currentTarget is null || currentTargetIndex>=currentPath.Count)
         {
-            currentTarget = targets[0];
+            currentTarget = currentPath[0];
             currentTargetIndex = 0;
         }
         else
-            currentTarget = targets[currentTargetIndex ++];
+            currentTarget = currentPath[currentTargetIndex ++];
+        
 
         _navMeshAgent.destination = currentTarget.position;
     }
+
+    List<Transform> changePath()
+    {
+        /*
+        pathChanging = true;
+        int a = 0;
+        while (true)
+        {
+            for(int i=0; i<nodesPerPath; i++)
+            {
+                if (currentPath[i] == nextPath[a])
+                {
+                    if (currentTargetIndex-i < nodesPerPath) return currentTargetIndex-i;
+                    else if(currentTargetIndex-i > nodesPerPath) return 
+                    
+                }
+            }
+
+            a++;
+        }
+        */
+        if (currentPath == path1) return path2;
+        else return path1;
+    }
+
+    void fillPaths()
+    {
+        path1.Add(targets[0]);
+        path1.Add(targets[1]);
+        path1.Add(targets[2]);
+        path2.Add(targets[1]);
+        path2.Add(targets[2]);
+        path2.Add(targets[3]);
+        currentPath = path2;
+    }
+
 }

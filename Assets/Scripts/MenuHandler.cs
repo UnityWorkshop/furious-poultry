0be14f7568831e15a557624212b7dc2c0a8f736d
SzyZu <skwarek116@gmail.com>
2024-02-06 18:00:27 +0100

    feat: dynamic amount of buttons

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,12 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
-    public void SwitchScene()
+    private int _scenes;
+    
+    [SerializeField] private GameObject buttonPrefab;
+
+    public void Start()
     {
-        SceneManager.LoadScene (sceneBuildIndex:1);
+        _scenes = EditorBuildSettings.scenes.Length;
+
+        for (int i = 0; i < _scenes; i++)
+        {
+            GameObject button;
+            button = Instantiate(buttonPrefab,this.transform);
+        }
     }
 }

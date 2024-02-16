0f17e6fa2eda7a87f554c360dec773c70bb3f823
I-Protecc <ryanbaumeister@googlemail.com>
2024-02-16 18:53:21 +0100

    refactor: applied OCP to poultry

---
+++ Assets/Scripts/Cluck.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace
+{
+    public class Cluck : Poultry
+    {
+        public override bool IsDead()
+        {
+            return isOnGround;
+        }
+        
+    }
+}

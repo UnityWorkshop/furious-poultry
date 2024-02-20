d6e5491e2461074bcf9343e200387279be23bd95
I-Protecc <ryanbaumeister@googlemail.com>
2024-02-20 17:47:40 +0100

    feat: beagle bullet spread for Primary Ability, untested

--- Assets/Scripts/Beagle.cs
+++ Assets/Scripts/Beagle.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Beagle : Poultry
 {
+    [SerializeField] private int pelletAmount = 9;
+    [SerializeField] private Rigidbody pelletPrefab;
+    [SerializeField] private float pelletSpeed = 1;// 1 = temp
+    [SerializeField] private float pelletSpread = 0.01f;
+        
+    
+    private bool _abilityUsed;
+    
+    
     public override bool IsDead()
     {
         return isOnGround;
@@ -11,12 +20,27 @@ public class Beagle : Poultry
 
     public override void DoPrimaryAbility()
     {
+        
         ShootShotgun();
+        
     }
 
     private void ShootShotgun()
     {
-        // pew pew here
-        // how do i shotgun?
+        for (int i = 0; i < pelletAmount; i++) // not optimal
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.x += RandomRotationDeviation();
+            rotation.y += RandomRotationDeviation();
+            rotation.z += RandomRotationDeviation();
+            Quaternion rotationQuaternion = Quaternion.Euler(rotation);
+            Rigidbody pellet = Instantiate(pelletPrefab, transform.position, rotationQuaternion);
+            pellet.AddForce(rotation * pelletSpeed);
+        }
+    }
+
+    private float RandomRotationDeviation()
+    {
+        return Random.Range(-pelletSpread, pelletSpread);
     }
 }

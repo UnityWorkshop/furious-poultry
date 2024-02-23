c805cfd0faff9bddc15428bee36a4ea2ee6a750a
dodofilet <tortenspam@outlook.com>
2024-02-23 18:18:51 +0100

    refactor: added pathmanager

--- Assets/Scripts/ZeplinAgent.cs
+++ Assets/Scripts/ZeplinAgent.cs
@@ -12,115 +12,42 @@ public class ZeplinAgent : MonoBehaviour
 {
     
     [SerializeField] private float stoppingDistance;
-    [SerializeField] private List<Path> paths; 
-    
+    [SerializeField] private List<Path> paths;
+
+    private PathManager _manager;
     
     private NavMeshAgent _navMeshAgent;
-    private Transform currentTarget;
-    private int currentTargetIndex;
-    private List<Transform> currentPath;
-    private int currentPathIndex;
-
-    private bool changingPaths;
-    private int ChangePathAt;
-    private int ChangePathAt2;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        if (!paths.Any())
-            throw new ArgumentException("you stupid");
-        ChangePath();
-        ChangeTarget();
+        _manager = new PathManager(paths);
+        _manager.GetNewTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (IsAgentAtTarget())
-        {
-            CheckPathChange();
-            ChangeTarget();
-        }
+            _navMeshAgent.destination = _manager.GetNewTarget().position;
+        
+        
 
         if (Input.GetKeyDown(KeyCode.A))
-        {
-            StartToChangePaths();
-        }
+            _manager.StartToChangePaths();
     }
-
-    void ChangeTarget()
-    {
-        
-        if (currentTarget is null || currentTargetIndex>=currentPath.Count)
-        {
-            currentTarget = currentPath[0];
-            currentTargetIndex = 0;
-        }
-        else
-            currentTarget = currentPath[currentTargetIndex ++];
-        
-
-        _navMeshAgent.destination = currentTarget.position;
-    }
-
-    List<Transform> nextPath()
-    {
-        List<Transform> nextPath;
-        if (currentPath is null || currentPathIndex>=paths.Count)
-        {
-            nextPath = paths[0].targets;
-        }
-        else
-            nextPath = paths[currentPathIndex+1].targets;
-
-        return nextPath;
-    }
-
-    void StartToChangePaths()
-    {
-        foreach (Transform target in currentPath)
-        {
-            foreach (Transform node in nextPath())
-            {
-                if (target == node)
-                {
-                    ChangePathAt = currentPath.IndexOf(target);
-                    ChangePathAt2 = currentPath.IndexOf(node);
-                }
-            }
-        }
-
-        changingPaths = true;
-    }
-
-    void CheckPathChange()
-    {
-        if (changingPaths)
-        {
-            if (ChangePathAt == currentTargetIndex)
-            {
-                ChangePath();
-                currentTargetIndex = ChangePathAt2;
-            } 
-        }
-    }
-
-    void ChangePath()
-    {
-        if (currentPath is null || currentPathIndex>=paths.Count)
-        {
-            currentPath = paths[0].targets;
-            currentPathIndex = 0;
-        }
-        else
-            currentPath = paths[currentPathIndex ++].targets;
-
-        changingPaths = false;
-    }
+    
 
     bool IsAgentAtTarget()
     {
-        return Vector3.Distance(transform.position, currentTarget.position) <= stoppingDistance;
+        return Vector3.Distance(transform.position, _navMeshAgent.destination) <= stoppingDistance;
+    }
+
+    public void SetDestination(Transform goal)
+    {
+        _navMeshAgent.destination = goal.position;
     }
 }

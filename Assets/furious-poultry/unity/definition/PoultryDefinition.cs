fa859317f8efeef336dd1ce3270e17cc47b4eb84
I-Protecc <ryanbaumeister@googlemail.com>
2024-02-27 18:26:04 +0100

    refactor: Poultry definition (incomplete)

---
+++ Assets/furious-poultry/unity/definition/PoultryDefinition.cs
@@ -0,0 +1,11 @@
+namespace furious_poultry.unity
+{
+    public class PoultryDefinition
+    { 
+        public int damage = 10;
+        public int health = 100;
+        public bool isOnGround;
+        public bool hasCollided;
+        public int decayTickDamage = 1; 
+    }
+}

0d9ae9d0152d75264f92a984ca0f388d7d4a1655
dodofilet <tortenspam@outlook.com>
2024-03-12 17:31:48 +0100

    fix: pellet instantiation logic

--- Assets/furious-poultry/unity/Pellet.cs
+++ Assets/furious-poultry/unity/Pellet.cs
@@ -1,3 +1,5 @@
+using System;
+using com.github.UnityWorkshop.furious_poultry.domain;
 using UnityEngine;
 
 namespace com.github.UnityWorkshop.furious_poultry.unity
@@ -5,10 +7,14 @@ namespace com.github.UnityWorkshop.furious_poultry.unity
     public class Pellet:MonoBehaviour
     {
         private Rigidbody body;
+        private float damage;
         
-        public Pellet(Rigidbody pelletPrefab,Transform shotPosition,Quaternion rotationQuaternion)
+        public Pellet(Rigidbody pelletPrefab,Vector3 shotPosition,Quaternion rotationQuaternion, float pelletdamage, float pelletSpeed)
         {
-            body = Instantiate(pelletPrefab, shotPosition.position, rotationQuaternion);
+            body = Instantiate(pelletPrefab, shotPosition, rotationQuaternion);
+            damage = pelletdamage;
+            Vector3 shotForce = transform.forward * pelletSpeed ;
+            AddForce(shotForce);
         }
 
         public void AddForce(Vector3 force)

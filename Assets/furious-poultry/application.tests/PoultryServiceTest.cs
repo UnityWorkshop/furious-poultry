e694ccd564d106c7c44cb18bc5c6c64aac99c116
SzyZu <skwarek116@gmail.com>
2024-03-15 17:59:32 +0100

    test: PoultryService CollidedWithEnemy

---
+++ Assets/furious-poultry/application.tests/PoultryServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using com.github.UnityWorkshop.furious_poultry.application.interfaces;
+using com.github.UnityWorkshop.furious_poultry.application.services;
+using com.github.UnityWorkshop.furious_poultry.domain;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace furious_poultry.application.tests
+{
+    public class PoultryServiceTest
+    {
+        [Test]
+        public void CollidedWithEnemy_NoDestroy_WhenNoDed()
+        {
+            var poultry = new Poultry(1, 10, 11, false, false);
+            var warthog = new Warthog(101234);
+
+            IDestructionProvider destructionProvider = Substitute.For<IDestructionProvider>();
+            PoultryService poultryService = new PoultryService(destructionProvider, poultry);
+            
+            poultryService.CollidedWithEnemy(warthog);
+            
+            destructionProvider.DidNotReceive().Destruct();
+        }
+        
+        [Test]
+        public void CollidedWithEnemy_Destroy_WhenDed()
+        {
+            var poultry = new Poultry(1, 10, 10, false, false);
+            var warthog = new Warthog(101234);
+
+            IDestructionProvider destructionProvider = Substitute.For<IDestructionProvider>();
+            PoultryService poultryService = new PoultryService(destructionProvider, poultry);
+            
+            poultryService.CollidedWithEnemy(warthog);
+            
+            destructionProvider.Received().Destruct();
+        }
+    }
+}
